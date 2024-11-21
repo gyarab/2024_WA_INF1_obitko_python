7b6cd09c56bb7519061d5490fe1578ecdf1d12fb
Jaromir007 <jaromir.obitko@student.gyarab.cz>
2024-11-21 11:09:38 +0100

    fix

--- test.py
+++ test.py
@@ -21,6 +21,4 @@ def fibonacci(n):
         fib_sequence = [0, 1]
         for i in range(2, n+1):
             fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
-        return fib_sequence[n-1]  
-
-fibonacci(0)
+        return fib_sequence[n]  
