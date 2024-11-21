68d4a5778499351191c5dbcea145a48fde5fc9d8
Jaromir007 <jaromir.obitko@student.gyarab.cz>
2024-11-21 10:57:52 +0100

    new task

--- test.py
+++ test.py
@@ -1,20 +1,7 @@
-class InputError(Exception):
-    pass
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
 
-def fibonacci(index):
-    if not isinstance(index, int) or index < 0:
-        raise InputError("Invalid input. Please provide a non-negative integer.")
-    elif index == 0:
-        return 0
-    elif index == 1:
-        return 1
-    else:
-        fib_sequence = [0, 1]
-        for i in range(2, index + 1):
-            fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
-        return fib_sequence[index]
-
-try:
-    print(fibonacci(0))  
-except InputError as e:
-    print(e)
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
